98020676f816593f701963158f5305128ecf666a
Valentin Dide <vali.dide@gmail.com>
2022-07-23 14:59:46 +0300

    IReadOnlyDictionary extension methods.

--- src/Common.Utilities/Extensions/Dictionary.cs
+++ src/Common.Utilities/Extensions/Dictionary.cs
@@ -16,7 +16,7 @@ public static class DictionaryExtensions
     /// <typeparam name="TValue">The value type.</typeparam>
     /// <returns>True if the value found in the dictionary is not null or false if it is not present in the dictionary or it's null.</returns>
     /// <remarks>The returned value is the value found in the dictionary if this is not null or the default value if it is not present in the dictionary or it's null.</remarks>
-    public static bool TryGetValueOrDefault<TKey, TValue>(this IDictionary<TKey, TValue> dictionary, TKey key, Func<TKey, TValue> defaultValue, out TValue value)
+    public static bool TryGetValueOrDefault<TKey, TValue>(this IReadOnlyDictionary<TKey, TValue> dictionary, TKey key, Func<TKey, TValue> defaultValue, out TValue value)
     {
         if (dictionary.TryGetValue(key, out var v) && v != null)
         {
@@ -37,13 +37,52 @@ public static class DictionaryExtensions
     /// <typeparam name="TKey">The key type.</typeparam>
     /// <typeparam name="TValue">The value type.</typeparam>
     /// <returns>The returned value is the value found in the dictionary if this is not null or the default value if it is not present in the dictionary or it's null.</returns>
-    public static TValue GetValueOrDefault<TKey, TValue>(this IDictionary<TKey, TValue> dictionary, TKey key, Func<TKey, TValue> defaultValue)
+    public static TValue GetValueOrDefault<TKey, TValue>(this IReadOnlyDictionary<TKey, TValue> dictionary, TKey key, Func<TKey, TValue> defaultValue)
+    {
+        _ = dictionary.TryGetValueOrDefault(key, defaultValue, out var v);
+        return v;
+    }
+    /// <summary>
+    /// Try to get the value specified by a key from a dictionary.
+    /// </summary>
+    /// <param name="dictionary">The dictionary.</param>
+    /// <param name="key">The key.</param>
+    /// <param name="defaultValue">The factory method for the default value to return.</param>
+    /// <param name="setDefault">Add the default value to the original dictionary for the given key.</param>
+    /// <param name="value">The returned value.</param>
+    /// <typeparam name="TKey">The key type.</typeparam>
+    /// <typeparam name="TValue">The value type.</typeparam>
+    /// <returns>True if the value found in the dictionary is not null or false if it is not present in the dictionary or it's null.</returns>
+    /// <remarks>The returned value is the value found in the dictionary if this is not null or the default value if it is not present in the dictionary or it's null.</remarks>
+    public static bool TryGetValueOrDefault<TKey, TValue>(this IDictionary<TKey, TValue> dictionary, TKey key, Func<TKey, TValue> defaultValue, bool setDefault, out TValue value)
     {
         if (dictionary.TryGetValue(key, out var v) && v != null)
         {
-            return v;
+            value = v;
+            return true;
         }
 
-        return defaultValue(key);
+        value = defaultValue(key);
+        if (setDefault)
+        {
+            dictionary.Add(key, value);
+        }
+        return false;
+    }
+
+    /// <summary>
+    /// Try to get the value specified by a key from a dictionary.
+    /// </summary>
+    /// <param name="dictionary">The dictionary.</param>
+    /// <param name="key">The key.</param>
+    /// <param name="defaultValue">The factory method for the default value to return.</param>
+    /// <param name="setDefault">Add the default value to the original dictionary for the given key.</param>
+    /// <typeparam name="TKey">The key type.</typeparam>
+    /// <typeparam name="TValue">The value type.</typeparam>
+    /// <returns>The returned value is the value found in the dictionary if this is not null or the default value if it is not present in the dictionary or it's null.</returns>
+    public static TValue GetValueOrDefault<TKey, TValue>(this IDictionary<TKey, TValue> dictionary, TKey key, Func<TKey, TValue> defaultValue, bool setDefault)
+    {
+        _ = dictionary.TryGetValueOrDefault(key, defaultValue, setDefault, out var v);
+        return v;
     }
 }
--- test/UnitTests/Extensions/Dictionary.Specs.cs
+++ test/UnitTests/Extensions/Dictionary.Specs.cs
@@ -65,4 +65,71 @@ public class DictionarySpecs
         var defaultValue = new TestClass { Id = "1" };
         Assert.Same(defaultValue, dictionary.GetValueOrDefault(1, _ => defaultValue));
     }
+
+    [Fact]
+    public void TryGetValueOrDefault_Should_Return_The_Item_If_It_Exists_Without_Setting_The_Value()
+    {
+        var dictionary = new Dictionary<int, TestClass>
+        {
+            {1, new TestClass { Id = "1" }}
+        };
+
+        Assert.True(dictionary.TryGetValueOrDefault(1, _ => null, false,out var v));
+        Assert.Same(dictionary[1], v);
+    }
+
+    [Fact]
+    public void TryGetValueOrDefault_Should_Return_Default_If_Missing_Without_Setting_The_Value()
+    {
+        var dictionary = new Dictionary<int, TestClass>();
+        var defaultValue = new TestClass {Id = "1"};
+        Assert.False(dictionary.TryGetValueOrDefault(1, _ => defaultValue, false, out var v));
+        Assert.Same(defaultValue, v);
+    }
+
+    [Fact]
+    public void TryGetValueOrDefault_Should_Return_Default_If_Null_Without_Setting_The_Value()
+    {
+        var dictionary = new Dictionary<int, TestClass> {{1, null}};
+        var defaultValue = new TestClass { Id = "1" };
+        Assert.False(dictionary.TryGetValueOrDefault(1, _ => defaultValue, false, out var v));
+        Assert.Same(defaultValue, v);
+    }
+
+    [Fact]
+    public void GetValueOrDefault_Should_Return_The_Item_If_It_Exists_Without_Setting_The_Value()
+    {
+        var dictionary = new Dictionary<int, TestClass>
+        {
+            {1, new TestClass { Id = "1" }}
+        };
+
+        Assert.Same(dictionary[1], dictionary.GetValueOrDefault(1, _ => null, false));
+    }
+
+    [Fact]
+    public void TryGetValueOrDefault_Should_Return_Default_If_Missing_With_Setting_The_Value()
+    {
+        var dictionary = new Dictionary<int, TestClass>();
+        var defaultValue = new TestClass {Id = "1"};
+        Assert.False(dictionary.TryGetValueOrDefault(1, _ => defaultValue, true, out var v));
+        Assert.Same(defaultValue, v);
+        Assert.Same(v, dictionary[1]);
+    }
+
+    [Fact]
+    public void GetValueOrDefault_Should_Return_Default_If_Missing_Without_Setting_The_Value()
+    {
+        var dictionary = new Dictionary<int, TestClass>();
+        var defaultValue = new TestClass {Id = "1"};
+        Assert.Same(defaultValue, dictionary.GetValueOrDefault(1, _ => defaultValue, false));
+    }
+
+    [Fact]
+    public void GetValueOrDefault_Should_Return_Default_If_Null_Without_Setting_The_Value()
+    {
+        var dictionary = new Dictionary<int, TestClass> {{1, null}};
+        var defaultValue = new TestClass { Id = "1" };
+        Assert.Same(defaultValue, dictionary.GetValueOrDefault(1, _ => defaultValue, false));
+    }
 }
