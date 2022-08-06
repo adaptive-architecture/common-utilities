c9e337750262265045851069cfb150d1cda70713
Valentin Dide <vali.dide@gmail.com>
2022-08-06 22:25:21 +0300

    Removed threshold

--- pipeline/unit-test.sh
+++ pipeline/unit-test.sh
@@ -2,8 +2,9 @@
 
 dotnet test \
   /p:CollectCoverage=true \
-  /p:Threshold=80 \
-  /p:ThresholdStat=total \
   /p:CoverletOutputFormat=\"json,lcov\"  \
   /p:CoverletOutput=\"../../coverage/\" \
   /p:MergeWith=\"../../coverage/coverage.json\"
+
+#  /p:Threshold=80 \
+#  /p:ThresholdStat=total \
