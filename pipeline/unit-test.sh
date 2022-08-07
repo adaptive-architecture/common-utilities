9ab32679ee72debdc562adda3abeaae17a1a55ee
Valentin Dide <vali.dide@gmail.com>
2022-08-07 08:42:33 +0300

    WIP

--- pipeline/unit-test.sh
+++ pipeline/unit-test.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 dotnet test \
-  /p:CollectCoverage=true \
+  /p:CollectCoverage=\"true\" \
   /p:CoverletOutputFormat=\"json,lcov\"  \
   /p:CoverletOutput=\"../../coverage/\" \
   /p:MergeWith=\"../../coverage/coverage.json\"
+  /p:Exclude=\"[*.IntegrationTests?]*\"
 
 #  /p:Threshold=80 \
 #  /p:ThresholdStat=total \
+#  /p:Exclude=\"[*.IntegrationTests?]*\" - exclude all types in an assembly ending with "IntegrationTests" or "IntegrationTest" (the ? makes the s optional)
---
+++ test/Common.Utilities.Redis.IntegrationTests/PubSub/RedisMessageHub.Int.cs
@@ -0,0 +1,90 @@
+﻿using AdaptArch.Common.Utilities.PubSub.Contracts;
+using AdaptArch.Common.Utilities.Redis.PubSub;
+using StackExchange.Redis;
+
+namespace AdaptArch.Common.Utilities.Redis.IntegrationTests.PubSub
+{
+    public class RedisMessageHubInt
+    {
+        public record MyMessage
+        {
+            public string Id { get; set; }
+        }
+
+        private readonly IMessageHub _messageHub;
+        private readonly IMessageHubAsync _messageHubAsync;
+
+        public RedisMessageHubInt()
+        {
+            var hub = new RedisMessageHub(ConnectionMultiplexer.Connect("127.0.0.1:6379"),
+                new RedisMessageHubOptions());
+            _messageHub = hub;
+            _messageHubAsync = hub;
+        }
+
+        [Fact]
+        public void Should_Subscribe_Publish_Unsubscribe()
+        {
+            MyMessage receivedMessage = null;
+            var subId = _messageHub.Subscribe<MyMessage>(nameof(MyMessage),
+                (m, _) =>
+                {
+                    receivedMessage = m.Data;
+                    return Task.CompletedTask;
+                });
+
+            Assert.Null(receivedMessage);
+
+            var sentMessage = new MyMessage { Id = Guid.NewGuid().ToString("N") };
+
+            _messageHub.Publish(nameof(MyMessage), sentMessage);
+            Thread.Sleep(TimeSpan.FromMilliseconds(10));
+
+            Assert.NotNull(receivedMessage);
+            Assert.Equal(sentMessage.Id, receivedMessage.Id);
+
+            _messageHub.Unsubscribe(subId);
+            receivedMessage = null;
+
+            _messageHub.Publish(nameof(MyMessage), sentMessage);
+            Thread.Sleep(TimeSpan.FromMilliseconds(10));
+
+            Assert.Null(receivedMessage);
+        }
+
+        [Fact]
+        public async Task Should_Subscribe_Publish_Unsubscribe_Async()
+        {
+            MyMessage receivedMessage = null;
+            var subId = await _messageHubAsync.SubscribeAsync<MyMessage>(nameof(MyMessage),
+                    (m, _) =>
+                    {
+                        receivedMessage = m.Data;
+                        return Task.CompletedTask;
+                    }, CancellationToken.None)
+                .ConfigureAwait(false);
+
+            Assert.Null(receivedMessage);
+
+            var sentMessage = new MyMessage { Id = Guid.NewGuid().ToString("N") };
+
+            await _messageHubAsync.PublishAsync(nameof(MyMessage), sentMessage, CancellationToken.None)
+                .ConfigureAwait(false);
+
+            await Task.Delay(TimeSpan.FromMilliseconds(10)).ConfigureAwait(false);
+
+            Assert.NotNull(receivedMessage);
+            Assert.Equal(sentMessage.Id, receivedMessage.Id);
+
+            await _messageHubAsync.UnsubscribeAsync(subId, CancellationToken.None).ConfigureAwait(false);
+            receivedMessage = null;
+
+            await _messageHubAsync.PublishAsync(nameof(MyMessage), sentMessage, CancellationToken.None)
+                .ConfigureAwait(false);
+
+            await Task.Delay(TimeSpan.FromMilliseconds(10)).ConfigureAwait(false);
+
+            Assert.Null(receivedMessage);
+        }
+    }
+}
