2eaae5f52fc89ae9bcbb06a712ed94f24c2abaae
Valentin Dide <vali.dide@gmail.com>
2023-11-16 17:17:55 +0200

    Tests

--- test/Common.Utilities.Redis.IntegrationTests/IntegrationJsonSerializerContext.cs
+++ test/Common.Utilities.Redis.IntegrationTests/IntegrationJsonSerializerContext.cs
@@ -1,6 +1,11 @@
 ﻿using System.Text.Json.Serialization;
+using AdaptArch.Common.Utilities.PubSub.Contracts;
+using static AdaptArch.Common.Utilities.Redis.IntegrationTests.PubSub.RedisMessageHubInt;
 
 namespace AdaptArch.Common.Utilities.Redis.IntegrationTests;
 
 [JsonSerializable(typeof(object))]
+[JsonSerializable(typeof(MyMessage))]
+[JsonSerializable(typeof(IMessage<object>))]
+[JsonSerializable(typeof(IMessage<MyMessage>))]
 internal partial class IntegrationJsonSerializerContext : JsonSerializerContext;
--- test/Common.Utilities.Redis.UnitTests/TestJsonSerializerContext.cs
+++ test/Common.Utilities.Redis.UnitTests/TestJsonSerializerContext.cs
@@ -1,6 +1,8 @@
 ﻿using System.Text.Json.Serialization;
+using AdaptArch.Common.Utilities.PubSub.Contracts;
 
 namespace AdaptArch.Common.Utilities.Redis.UnitTests;
 
 [JsonSerializable(typeof(object))]
+[JsonSerializable(typeof(IMessage<object>))]
 internal partial class TestJsonSerializerContext : JsonSerializerContext;
