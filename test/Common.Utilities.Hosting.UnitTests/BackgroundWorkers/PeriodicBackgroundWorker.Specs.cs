17fd1c51df7a0a65b57508161f75449686a43fed
Valentin Dide <vali.dide@gmail.com>
2023-12-30 17:49:12 +0200

    Clean-up and unit tests

--- test/Common.Utilities.Hosting.UnitTests/BackgroundWorkers/PeriodicBackgroundWorker.Specs.cs
+++ test/Common.Utilities.Hosting.UnitTests/BackgroundWorkers/PeriodicBackgroundWorker.Specs.cs
@@ -8,100 +8,154 @@ using AdaptArch.Common.Utilities.Hosting.BackgroundWorkers.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Configuration.Memory;
 
-
 namespace AdaptArch.Common.Utilities.Hosting.UnitTests.BackgroundWorkers;
 
 public class PeriodicBackgroundWorkerSpecs
 {
-    private static readonly TimeSpan ShortJobDuration = TimeSpan.FromMicroseconds(50);
-    private static readonly TimeSpan StandardPeriod = TimeSpan.FromSeconds(1);
-    private static readonly TimeSpan StandardDelay = TimeSpan.FromSeconds(5);
-
-    private readonly ServiceCollection _serviceCollection;
-    private readonly IConfigurationBuilder _configurationBuilder = new ConfigurationBuilder()
-        .AddInMemoryCollection(new Dictionary<string, string>
-        {
-            { $"periodicWorkers:{nameof(PeriodicWorkerConfiguration.Enabled)}", Boolean.TrueString },
-            { $"periodicWorkers:{nameof(PeriodicWorkerConfiguration.Period)}", StandardPeriod.ToString("c") },
-            { $"periodicWorkers:{nameof(PeriodicWorkerConfiguration.InitialDelay)}", StandardDelay.ToString("c") },
-        });
-
-    private readonly MemoryConfigurationProvider _memoryConfigurationProvider;
-
-    public class JobState
+    [Theory]
+    [InlineData(100, 5_000, 1_000)]
+    [InlineData(2_000, 5_000, 1_000)]
+    public async Task Should_Execute_The_Job(int executionDurationMs, int initialDelayMs, int periodMs)
     {
-        public int ExecutionCount;
-    }
+        using var cts = new CancellationTokenSource();
 
-    public class ShortJob : IJob
-    {
-        private readonly JobState _jobState;
+        var state = new JobState(TimeSpan.FromMilliseconds(executionDurationMs),
+            TimeSpan.FromMilliseconds(initialDelayMs),
+            TimeSpan.FromMilliseconds(periodMs));
+        var serviceProvider = await BeginTestAsync(state, Boolean.TrueString, cts.Token);
 
-        public ShortJob(JobState jobState) => _jobState = jobState;
-
-        public Task ExecuteAsync(CancellationToken cancellationToken)
-        {
-            _jobState.ExecutionCount++;
-            return Task.CompletedTask;
-        }
-    }
-
-    public PeriodicBackgroundWorkerSpecs()
-    {
-        var configuration = _configurationBuilder.Build();
-        _memoryConfigurationProvider = configuration.Providers.OfType<MemoryConfigurationProvider>().Single();
-
-        _serviceCollection = new ServiceCollection();
-        _serviceCollection
-            .AddSingleton(configuration)
-            .AddSingleton<IConfiguration>(configuration)
-            .AddOptions<PeriodicWorkerConfiguration>().BindConfiguration("periodicWorkers");
-
-        _serviceCollection
-            .AddSingleton<ILoggerFactory, NullLoggerFactory>()
-            .AddSingleton(typeof(ILogger<>), typeof(NullLogger<>))
-            .AddSingleton<JobState>()
-            .AddPeriodicBackgroundJobs()
-            .WithPeriodicJob<ShortJob>();
-    }
-
-    [Fact]
-    public async Task Should_Execute_The_Job()
-    {
-        var serviceProvider = await BeginTestAsync();
-        var start = DateTime.UtcNow;
-        var state = serviceProvider.GetRequiredService<JobState>();
-        var iterationDuration = ShortJobDuration + StandardPeriod;
-
-        while (DateTime.UtcNow - start < StandardDelay)
+        while (state.Elapsed < state.InitialDelay)
         {
             // While the delay is not over, the job should not have executed.
-            Assert.Equal(GetEstimatedExecutionCount(ShortJobDuration, start), state.ExecutionCount);
-            await Task.Delay(iterationDuration);
+            Assert.Equal(state.GetEstimatedExecutionCount(), state.ExecutionCount);
+            await Task.Delay(state.ExecutionTime);
         }
 
         for (var i = 0; i < 3; i++)
         {
             // Now it should be equal to `i` as it should have executed
-            Assert.Equal(GetEstimatedExecutionCount(ShortJobDuration, start), state.ExecutionCount);
-            await Task.Delay(iterationDuration);
+            Assert.Equal(state.GetEstimatedExecutionCount(), state.ExecutionCount);
+            await Task.Delay(state.ExecutionTime);
         }
 
-        await EndTestAsync(serviceProvider);
-        var stopDateTime = DateTime.UtcNow;
-        var finalEstimate = GetEstimatedExecutionCount(ShortJobDuration, start, stopDateTime);
+        await EndTestAsync(state, serviceProvider, cts.Token);
+        cts.Cancel();
+
+        var finalEstimate = state.GetEstimatedExecutionCount();
 
         for (var i = 0; i < 3; i++)
         {
             // Now it should not advance anymore as the job has been stopped.
-            await Task.Delay(iterationDuration);
+            await Task.Delay(state.ExecutionTime);
             Assert.Equal(finalEstimate, state.ExecutionCount);
         }
     }
 
-    private async Task<IServiceProvider> BeginTestAsync(CancellationToken cancellationToken = default)
+    [Fact]
+    public async Task Should_Support_Disabling_After_Starting()
     {
-        var serviceProvider = _serviceCollection.BuildServiceProvider(new ServiceProviderOptions
+        using var cts = new CancellationTokenSource();
+
+        var state = new JobState(TimeSpan.FromMilliseconds(100),
+            TimeSpan.FromMilliseconds(2_000),
+            TimeSpan.FromMilliseconds(500));
+        var serviceProvider = await BeginTestAsync(state, Boolean.TrueString, cts.Token);
+
+        var configuration = serviceProvider.GetRequiredService<IConfigurationRoot>();
+        var memoryConfigurationProvider = configuration.Providers.OfType<MemoryConfigurationProvider>().Single();
+        memoryConfigurationProvider.Set($"periodicWorkers:{nameof(PeriodicWorkerConfiguration.Enabled)}", Boolean.FalseString);
+        configuration.Reload();
+
+        while (state.Elapsed < TimeSpan.FromMilliseconds(5_000))
+        {
+            // While the delay is not over, the job should not have executed.
+            Assert.Equal(0, state.ExecutionCount);
+            await Task.Delay(state.ExecutionTime);
+        }
+
+        await EndTestAsync(state, serviceProvider, cts.Token);
+        cts.Cancel();
+
+        for (var i = 0; i < 3; i++)
+        {
+            // Now it should not advance anymore as the job has been stopped.
+            await Task.Delay(state.ExecutionTime);
+            Assert.Equal(0, state.ExecutionCount);
+        }
+    }
+
+    [Fact]
+    public async Task Should_Support_Enabling_After_Starting()
+    {
+        using var cts = new CancellationTokenSource();
+
+        var state = new JobState(TimeSpan.FromMilliseconds(100),
+            TimeSpan.FromMilliseconds(5_000),
+            TimeSpan.FromMilliseconds(1_000));
+        var serviceProvider = await BeginTestAsync(state, Boolean.FalseString, cts.Token);
+
+        var configuration = serviceProvider.GetRequiredService<IConfigurationRoot>();
+        var memoryConfigurationProvider = configuration.Providers.OfType<MemoryConfigurationProvider>().Single();
+        memoryConfigurationProvider.Set($"periodicWorkers:{nameof(PeriodicWorkerConfiguration.Enabled)}", Boolean.TrueString);
+        configuration.Reload();
+
+        // Call `Start` again to refresh the state's internal start time.
+        state.Start();
+
+        while (state.Elapsed < state.InitialDelay)
+        {
+            // While the delay is not over, the job should not have executed.
+            Assert.Equal(state.GetEstimatedExecutionCount(), state.ExecutionCount);
+            await Task.Delay(state.ExecutionTime);
+        }
+
+        for (var i = 0; i < 3; i++)
+        {
+            // Now it should be equal to `i` as it should have executed
+            Assert.Equal(state.GetEstimatedExecutionCount(), state.ExecutionCount);
+            await Task.Delay(state.ExecutionTime);
+        }
+
+        await EndTestAsync(state, serviceProvider, cts.Token);
+        cts.Cancel();
+
+        var finalEstimate = state.GetEstimatedExecutionCount();
+
+        for (var i = 0; i < 3; i++)
+        {
+            // Now it should not advance anymore as the job has been stopped.
+            await Task.Delay(state.ExecutionTime);
+            Assert.Equal(finalEstimate, state.ExecutionCount);
+        }
+    }
+
+    private static async Task<IServiceProvider> BeginTestAsync(JobState state, string enabled, CancellationToken cancellationToken = default)
+    {
+        var configurationBuilder = new ConfigurationBuilder()
+        .AddInMemoryCollection(new Dictionary<string, string>
+        {
+            { $"periodicWorkers:{nameof(PeriodicWorkerConfiguration.Enabled)}", enabled },
+            { $"periodicWorkers:{nameof(PeriodicWorkerConfiguration.Period)}", state.Period.ToString("c") },
+            { $"periodicWorkers:{nameof(PeriodicWorkerConfiguration.InitialDelay)}", state.InitialDelay.ToString("c") },
+        });
+
+        var configuration = configurationBuilder.Build();
+
+        var serviceCollection = new ServiceCollection();
+
+        serviceCollection
+            .AddSingleton(configuration)
+            .AddSingleton<IConfiguration>(configuration)
+            .AddOptions<PeriodicWorkerConfiguration>().BindConfiguration("periodicWorkers");
+
+        serviceCollection
+            .AddSingleton<ILoggerFactory, NullLoggerFactory>()
+            .AddSingleton(typeof(ILogger<>), typeof(NullLogger<>))
+            .AddSingleton(state)
+            .AddPeriodicBackgroundJobs()
+            .WithPeriodicJob<ShortJob>();
+
+        var serviceProvider = serviceCollection.BuildServiceProvider(new ServiceProviderOptions
         {
             ValidateOnBuild = true,
             ValidateScopes = true
@@ -112,26 +166,85 @@ public class PeriodicBackgroundWorkerSpecs
             await hostedService.StartAsync(cancellationToken);
         }
 
+        state.Start();
+
         return serviceProvider;
     }
 
-    private async Task EndTestAsync(IServiceProvider serviceProvider, CancellationToken cancellationToken = default)
+    private static async Task EndTestAsync(JobState state, IServiceProvider serviceProvider, CancellationToken cancellationToken = default)
     {
         foreach (var hostedService in serviceProvider.GetServices<IHostedService>())
         {
             await hostedService.StopAsync(cancellationToken);
         }
+        state.Stop();
     }
 
-    private static int GetEstimatedExecutionCount(TimeSpan jobDuration, DateTime start, DateTime? stopDateTime = null)
+    public class JobState
     {
-        var end = stopDateTime ?? DateTime.UtcNow;
-        var runningTime = end - start - StandardDelay;
-        if (runningTime < TimeSpan.Zero)
+        public int ExecutionCount { get; private set; }
+        public TimeSpan Duration { get; init; }
+        public TimeSpan InitialDelay { get; init; }
+        public TimeSpan Period { get; init; }
+        public TimeSpan ExecutionTime { get; internal set; }
+
+        private DateTime? _start;
+        private DateTime? _stop;
+        private readonly TimeSpan _contextSwitchingTime = TimeSpan.FromMilliseconds(31);
+
+        public JobState(TimeSpan duration, TimeSpan initialDelay, TimeSpan period)
         {
-            return 0;
+            Duration = duration;
+            InitialDelay = initialDelay;
+            Period = period;
+
+            // The timer will always fire at it's specified period, regardless of the job duration.
+            // https://github.com/dotnet/runtime/issues/95238#issuecomment-1826758659
+            ExecutionTime = Period + _contextSwitchingTime;
         }
 
-        return (int)Math.Ceiling(runningTime / (StandardPeriod + jobDuration));
+        public void Start() => _start = DateTime.UtcNow;
+        public void Stop() => _stop = DateTime.UtcNow;
+
+        public TimeSpan Elapsed => _start.HasValue ? DateTime.UtcNow - _start.Value : TimeSpan.Zero;
+
+        public async Task IncrementAsync(CancellationToken cancellationToken)
+        {
+            await Task.Delay(Duration, cancellationToken);
+            ExecutionCount++;
+        }
+
+        public int GetEstimatedExecutionCount()
+        {
+            var end = _stop ?? DateTime.UtcNow;
+            var start = _start ?? DateTime.UtcNow;
+
+            // If the job takes longer than the period, it will be executed again immediately.
+            // We still need to wait for the job to finish the first execution before we can start the second.
+            var expectedExecutionTime = _contextSwitchingTime + (Duration > Period
+                ? Duration
+                : Period);
+
+            // Context switching between threads can cause the job to take longer than the "expected" time.
+            var runningTime = end - start - InitialDelay - Duration - _contextSwitchingTime;
+            if (runningTime <= TimeSpan.Zero)
+            {
+                return 0;
+            }
+
+            return (int)Math.Ceiling(runningTime / expectedExecutionTime);
+        }
+    }
+
+    public class ShortJob : IJob
+    {
+        private readonly JobState _jobState;
+
+        public ShortJob(JobState jobState) => _jobState = jobState;
+
+        public Task ExecuteAsync(CancellationToken cancellationToken)
+        {
+            return _jobState.IncrementAsync(cancellationToken);
+        }
     }
 }
