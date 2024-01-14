6c3eef7c7d42d32c879d92a381cf3911ba287761
Valentin Dide <vali.dide@gmail.com>
2024-01-14 16:47:47 +0200

    Formatting

--- src/Common.Utilities/Encoding/Base32.cs
+++ src/Common.Utilities/Encoding/Base32.cs
@@ -1,4 +1,4 @@
-namespace AdaptArch.Common.Utilities.Encoding;
+﻿namespace AdaptArch.Common.Utilities.Encoding;
 
 /// <summary>
 /// Contains utility APIs to assist with encoding and decoding operations using <see href="https://datatracker.ietf.org/doc/html/rfc4648#section-6">Base 32</see>.
